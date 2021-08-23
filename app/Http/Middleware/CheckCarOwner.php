ee5b94308cd8d8d3d2690611da5bdfb71580bf38
maniek-maniak <markowcpw@gmail.com>
2021-08-23 22:37:15 +0200

    Check visits in CheckCarOwner

--- app/Http/Middleware/CheckCarOwner.php
+++ app/Http/Middleware/CheckCarOwner.php
@@ -6,8 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-use App\Models\Car;
+use Illuminate\Support\Facades\DB;
 
 class CheckCarOwner
 {
@@ -20,11 +19,16 @@ class CheckCarOwner
      */
     public function handle(Request $request, Closure $next)
     {
-        $car_id = $request->route()->car_id;
-        $visit_id = $request->route()->visit_id;
+        $visit_id = $request->route()->visit_id;      
+        $visit_car_id = DB::table('visits')->where('id',$visit_id)->value('car_id');
+        if ($visit_car_id <> '2'){
+            return redirect('/visits');
+        }
+        
         $zalogowany = Auth::user();
         $listCars = $zalogowany['cars'];
         $my_car = false;
+        $car_id = $request->route()->car_id;
         foreach ($listCars as $car){
             if ($car['id'] == $car_id){
                 $my_car = true;
